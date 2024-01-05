874b0ad8f798dfaef4e2d3e1a90581e662f12917
Gritsenko Vladislav <gricenkovlad007@gmail.com>
2024-01-05 12:11:16 +0300

    test collision

--- main.py
+++ main.py
@@ -1,21 +1,88 @@
+import os
+import random
 import sys
 import pygame
 
+pygame.init()
+pygame.display.set_caption("PyGame Quest")
+size = width, height = 800, 400
+screen = pygame.display.set_mode(size)
+all_sprites = pygame.sprite.Group()
+horizontal_borders = pygame.sprite.Group()
+vertical_borders = pygame.sprite.Group()
+
+
+def load_image(name, colorkey=None):
+    fullname = os.path.join('data', name)
+    if not os.path.isfile(fullname):
+        print(f"Файл с изображением '{fullname}' не найден")
+        sys.exit()
+    image = pygame.image.load(fullname)
+    if colorkey is not None:
+        image = image.convert()
+        if colorkey == -1:
+            colorkey = image.get_at((0, 0))
+        image.set_colorkey(colorkey)
+    else:
+        image = image.convert_alpha()
+    return image
+
+
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self, radius, x, y):
+        super().__init__(all_sprites)
+        self.radius = radius
+        self.image = load_image("DeftSorceress.png")
+        self.rect = self.image.get_rect()
+        self.rect.x = 100
+        self.rect.y = 100
+        self.vx = random.randint(-5, 5)
+        self.vy = random.randrange(-5, 5)
+
+    def update(self):
+        self.rect = self.rect.move(self.vx, self.vy)
+        if pygame.sprite.spritecollideany(self, horizontal_borders):
+            self.vy = -self.vy
+        if pygame.sprite.spritecollideany(self, vertical_borders):
+            self.vx = -self.vx
+
+
+class Border(pygame.sprite.Sprite):
+    def __init__(self, x1, y1, x2, y2):
+        super().__init__(all_sprites)
+        if x1 == x2:  # вертикальная стенка
+            self.add(vertical_borders)
+            self.image = pygame.Surface([1, y2 - y1])
+            self.rect = pygame.Rect(x1, y1, 1, y2 - y1)
+        else:  # горизонтальная стенка
+            self.add(horizontal_borders)
+            self.image = pygame.Surface([x2 - x1, 1])
+            self.rect = pygame.Rect(x1, y1, x2 - x1, 1)
+
 
 def main():
-    pygame.init()
-    pygame.display.set_caption("Квест")
-    size = width, height = 1920, 1080
-    screen = pygame.display.set_mode(size)
-
+    Border(5, 5, width - 5, 5)
+    Border(5, height - 5, width - 5, height - 5)
+    Border(5, 5, 5, height - 5)
+    Border(width - 5, 5, width - 5, height - 5)
+    for i in range(10):
+        Player(20, 100, 100)
     running = True
     fps = 60
     clock = pygame.time.Clock()
     while running:
-        screen.fill((0, 0, 0))
+        screen.fill((255, 255, 255))
+        event_key = None
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+            if event.type == pygame.KEYDOWN:
+                event_key = event
+
+        all_sprites.draw(screen)
+        all_sprites.update()
+
         clock.tick(fps)
         pygame.display.flip()
     pygame.quit()
